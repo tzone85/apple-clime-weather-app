cbe9fb8b82efdb84cc52b8bbf0eedb56fe8cefb9
thando <tzone85@gmail.com>
2017-11-29 17:58:55 +0200

    Included the location core libraries, extended the view controller to CLLocationManagerDelegate and then assigned delegate to self with a reasonable accuracy distance.
    added two new property list items for the poppups for the user permissions

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -7,17 +7,18 @@
 //
 
 import UIKit
+import CoreLocation
 
-
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    //let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    let APP_ID = "1ecc5c42d35d24d19ead5f81abacb492"
     
 
     //TODO: Declare instance variables here
-    
+    let locationManager = CLLocationManager()
 
     
     //Pre-linked IBOutlets
@@ -31,8 +32,9 @@ class WeatherViewController: UIViewController {
         
         
         //TODO:Set up the location manager here.
-    
-        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
         
     }
     
